5799d419903e12ff60e3ab02394003c41d505a93
devsomsak <devrleang@gmail.com>
2024-01-17 08:29:01 +0700

    feat:solve problem-solving-3 ex1-1x2 finished

--- ex-1.js
+++ ex-1.js
@@ -1,7 +1,18 @@
 // Exercise 1: Length of Last Word
-
 const lengthOfLastWord = function (s) {
-  //Start coding here
+  let lengthLastWordCount = 0;
+  let foundCharecter = false;
+  for (let i = s.length - 1; i >= 0; i--) {
+    console.log(s.length);
+    if (s[i] !== " ") {
+      lengthLastWordCount++;
+      foundCharecter = true;
+      console.log(lengthLastWordCount);
+    } else if (foundCharecter) {
+      break;
+    }
+  }
+  return lengthLastWordCount;
 };
 
 const result1 = lengthOfLastWord("Hello World");
--- ex-2.js
+++ ex-2.js
@@ -1,7 +1,25 @@
 // Exercise 2: Single Number
 
-const singleNumber = function (nums) {
-  // Start coding here
+const singleNumber = function (number) {
+  const arrayCount = new Array(Math.max(...number) + 1).fill(0);
+  console.log(arrayCount);
+
+  //[4, 1, 2, 1, 2]
+  for (let i = 0; i < number.length; i++) {
+    // index 0 ,Value in Array(4) ->index Array[4] to add 1
+    arrayCount[number[i]]++;
+    console.log(number[i]);
+    console.log(arrayCount);
+  }
+
+  for (let i = 0; i < arrayCount.length; i++) {
+    if (arrayCount[i] === 1) {
+      console.log(i);
+      return i;
+    }
+  }
+
+  return 0;
 };
 
 const result1 = singleNumber([2, 2, 1]);
